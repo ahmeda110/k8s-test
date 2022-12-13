264ab25d536e6bb9c38ab1beab7214bff3fb9f0f
Ahmed-Abbas <65793945+ahmeda110@users.noreply.github.com>
2022-12-13 11:36:24 -0700

    Added my project

---
+++ node-app2/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+ res.json({message: "hello from second app!"});
+});
+
+app.listen(3000, () => {
+ console.log('Server started on port 3000');
+});
